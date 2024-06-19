0e1b6e3a8db95844e0dda28423583f41e57fa5ba
qkrthdus605 <qkrthdus605@gmail.com>
2024-06-19 22:42:55 +0900

    setting: eslint rule 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig, { rules } from 'eslint-plugin-react';
 
-import { fixupConfigRules } from "@eslint/compat";
+import { fixupConfigRules } from '@eslint/compat';
 
 export default [
   {
@@ -14,19 +14,33 @@ export default [
   pluginJs.configs.recommended,
   tseslint.configs.recommended,
   {
-    files: ["**/*.jsx"],
+    files: ['**/*.jsx'],
     languageOptions: {
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
         },
       },
+      rules: {
+        'prettier/prettier': [
+          'error',
+          {
+            endOfLine: 'auto',
+          },
+        ],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+      },
     },
   },
   {
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
